7d181e524bcc5f241c07216cbc77795716a64da1
tarheel300 <andypguice@gmail.com>
2024-01-23 00:45:41 -0500

    Adding In Mortgage Object to JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("hello world!")
+
+
+class Mortgage {
+    constructor(interestRateBps, startDate, principalCents) {
+        this.interestRateBps = interestRateBps;
+        this.startDate = startDate;
+        this.principalCents = principalCents;
+    }
+
+    toString() {
+        return `Mortgage is for \$${this.principalCents / 100}, with a ${this.interestRateBps / 100}% interest rate, starting ${this.startDate}.`
+    }
+}
+
+const myMortgage = new Mortgage(275, '2024-01-22', '25200000');
+
+console.log(myMortgage.toString());
+
+
+
+
+
+
+
+
+
+
